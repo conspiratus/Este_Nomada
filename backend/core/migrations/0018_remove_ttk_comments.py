3f38d6742093cb1f96ba64b2392e7cc9c1d9098d
Conspiratus <Conspiratus@outlook.com>
2025-11-26 15:14:42 +0100

    Fix: remove duplicate migration and fix dependencies

--- backend/core/migrations/0018_remove_ttk_comments.py
+++ backend/core/migrations/0018_remove_ttk_comments.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0018_ttkcomment_ttkversionhistory'),
+        ('core', '0017_dishttk'),
     ]
 
     operations = [
